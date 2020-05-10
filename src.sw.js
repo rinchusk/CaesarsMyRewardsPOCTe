8a986c8a1b0b1083b3f521aafebcc51430ac82b8
rkoshy <Rinchu.Koshy.contractor@hilton.com>
2020-05-09 21:30:17 -0500

    Updates

---
+++ src.sw.js
@@ -0,0 +1,25 @@
+importScripts("/precache-manifest.db69a4af05bcc45482486611c09e14e1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
+console.log('Service worker Loaded ');
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+self.addEventListener('push', e => {
+    console.log('Inside Push ');
+const data =  e.data.json();
+//const data =  e.data;
+console.log('Push Received15 ');
+console.log(data);
+    /* self.registration.showNotification(data.title,{
+        body : " Sucess "
+    }); */
+    self.registration.showNotification(data.data.title,{
+        body : " Sucess "
+    });
+});
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+
+
+
